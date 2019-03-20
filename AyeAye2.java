894ccc9692b163ebbb5cfd9ee5c454aca2afdbf9
lampperson <31216138+lampperson@users.noreply.github.com>
2019-03-20 11:15:20 -0700

    Add files via upload

---
+++ AyeAye2.java
@@ -0,0 +1,19 @@
+public class AyeAye2 extends AyeAye
+{
+    public AyeAye2(String name, String desc)
+    {
+        super(name, desc);
+    }
+    @Override
+    public String eat()
+    {return "The Aye-Aye plunges it's long middle finger inside of the tree and uses it to fish for insects to eat. Except he gets less then all the other Aye-Ayes";}
+    @Override
+    public String climb()
+    {return "The Aye-Aye climbs on the branch of a tree, then immediately falls off because he has a loose grip.";}
+    @Override
+    public String walk()
+    {return "The Aye-Aye beguins walking around on four legs, then immediatly trips and falls because he is not well coordinated.";}
+    @Override 
+    public String makeNoise()
+    {return "Someone accidently shines a light in the Aye-Ayes sensitive eyes, startled, the Aye-Aye makes a wierd noise that sound like metal scraping on metal.\n The Aye-Aye is now blinded for life because of how weak his eyes were.";}
+}
