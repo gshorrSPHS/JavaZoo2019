2edbf1018627ad4683c9341c703641b03f29b489
JoKoyama <31217152+JoKoyama@users.noreply.github.com>
2019-03-20 11:13:23 -0700

    Update PossibleyAFerret.java

--- PossibleyAFerret.java
+++ PossibleyAFerret.java
@@ -1,6 +1,6 @@
-public class PossibleyAFerret extends Ferret implements Walking
+public class PossiblyAFerret extends Ferret implements Walking
 {
-  public PossibleyAFerret(String n,String i){super(n,i);} 
+  public PossiblyAFerret(String n,String i){super(n,i);} 
   @Override public String eat(){return"The ferret gnaws on some chiken wings";}
   @Override public String play(){return "it looks at you \"but why would you force it to have fun?\" and goes back to resting ";}
   @Override public String makeNoise(){return "dook dook";}
