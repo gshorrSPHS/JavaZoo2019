1dde3f989dfb1e0ab90d821a59f199a82d7fd8da
echoi329 <48666748+echoi329@users.noreply.github.com>
2019-03-20 11:22:11 -0700

    Add files via upload

--- InflatedBlobfish.java
+++ InflatedBlobfish.java
@@ -1,16 +1,14 @@
 public class InflatedBlobfish extends Blobfish implements Flying
 {
-    private String air;
     
-    public InflatedBlobfish(String name, String desc, String air)
+    public InflatedBlobfish(String name, String desc)
     {
         super(name, desc);
-        this.air = air;
     }
 
     @Override
     public String fly()
     {
-        return "The blobfish will fly after being inflated from air.";
+        return "*Flies away*";
     }
 }
